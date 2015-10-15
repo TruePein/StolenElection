77ae6cc87bc2ad5c2eda91c8edcc1fa1e943476e
TruePein <stoneseric_21@ymail.com>
2015-10-14 18:28:46 -0700

    Added evote
    Added the evote repo

---
+++ Voting Systems/evote/models/db_votes.py
@@ -0,0 +1,54 @@
+not_empty = IS_NOT_EMPTY()
+
+db.define_table(
+    'election',
+    Field('title',requires=not_empty),
+    Field('ballot_model','text',requires=not_empty), # empty ballot
+    Field('voters','text',requires=not_empty),
+    Field('managers','text',requires=not_empty),
+    Field('deadline','datetime'),
+    Field('vote_email','text'),
+    Field('voted_email','text'),
+    Field('email_sender',requires=IS_EMAIL(),default=EMAIL_SENDER,writable=False),
+    Field('not_voted_email','text'),
+    Field('public_key',writable=False,readable=False),                
+    Field('private_key',writable=False,readable=False),                
+    Field('counters','text',writable=False,readable=False),                
+    Field('closed','boolean',writable=False,readable=False),
+    auth.signature,
+    format='%(title)s')
+
+db.define_table(
+    'voter',
+    Field('voter_uuid',writable=False,readable=False),
+    Field('election_id',db.election,writable=False),
+    Field('email',requires=IS_EMAIL()),
+    Field('voted','boolean',default=False),
+    Field('invited_on','datetime'))
+
+db.define_table(
+    'ballot',
+    Field('election_id',db.election),
+    Field('ballot_content','text'),  # voted or blank ballot
+    Field('assigned','boolean',default=False),
+    Field('voted','boolean',default=False),
+    Field('voted_on','datetime',default=None),
+    Field('results','text',default='{}'),
+    Field('ballot_uuid'), # uuid embedded in ballot
+    Field('signature')) # signature of ballot (voted or blank)
+
+
+# if resticted to some users get the list of users
+def load_users_emails(filename=myconf.take('app.users_filename')):
+    import urllib, os
+    if not filename:
+        return None
+    elif filename.startswith('http://') or filename.startswith('https://'):
+        emails = urllib.urlopen(filename).read().split('\n')
+    else:
+        if not filename.startswith('/'):
+            filename = os.path.join(request.folder, filename)
+        emails = open(filename).read().split('\n')
+    emails = [email.strip() for email in emails if email.strip()]
+    return emails
+users_emails = cache.ram('users_emails',load_users_emails,3600)
